abd5f4026d3e57e36f990d023f358ae3057a0891
jishnu2021 <jishnughosh98@gmail.com>
2025-04-14 11:38:50 +0530

    add backend link

--- telemed-connect-ai-care/src/components/settings/DoctorSetting.jsx
+++ telemed-connect-ai-care/src/components/settings/DoctorSetting.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Navbar from '../../components/Navbar'
 
-const API_URL = "http://localhost:5000"
+const API_URL = "https://telemed-connect-backend.onrender.com"
 const DoctorSetting = () => {
   const [doctorData, setDoctorData] = useState(null);
   const [email, setEmail] = useState('');
